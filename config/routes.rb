b5448c1326e9f50f48111d2595914382e6ab5ee9
marcoverzero <marcoverzero@gmail.com>
2017-07-20 11:07:06 -0600

    allows user to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create, :show]
+
+  root 'users#show'
+
   get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
 
   get '/dashboard/:id', to: "users#dashboard", as: "dashboard_user"
 
-  post '/login', to: "sessions#create"
 
 end
--- spec/features/user_logs_in_and_out_spec.rb
+++ spec/features/user_logs_in_and_out_spec.rb
@@ -5,14 +5,14 @@ RSpec.feature "User can login" do
     user_attributes = { name: "Bob", email: "bob@example.com", password: "password"}
 
     user = User.create(user_attributes)
-
     visit login_path
 
     fill_in "session[name]", with: user_attributes[:name]
     fill_in "session[email]", with: user_attributes[:email]
     fill_in "session[password]", with: user_attributes[:password]
-    click_on "Login"
+    save_and_open_page
 
+    click_on "Login"
 
     expect(current_path).to eq(dashboard_user_path(user))
     expect(page).to have_content("Welcome #{user.name}")
